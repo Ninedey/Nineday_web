90ecf9c875f4380ac0ff8b41e6dff4450754ad67
Ninedey <jimmy01054@gmail.com>
2025-01-08 14:51:07 +0800

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1> Nineday`s Flask App </h1>"
+    return "<h1> Nineday`s Flask App and Hello world</h1>"
 if __name__ == '__main__':
     app.run()
     app.run()
